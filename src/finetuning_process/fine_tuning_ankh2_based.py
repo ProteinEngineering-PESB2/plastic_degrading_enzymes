e56a8dd8d215a297bf45f50d56ba91525c8e7f56
dMedinaO <david.medina@cebib.cl>
2024-12-06 15:32:23 +0100

    adding gitignore

--- src/finetuning_process/fine_tuning_ankh2_based.py
+++ src/finetuning_process/fine_tuning_ankh2_based.py
@@ -7,7 +7,11 @@ from datasets import Dataset
 import pandas as pd
 import sys
 from json import dump
+from sklearn.utils import shuffle
 import gc
+
+import argparse
+parser = argparse.ArgumentParser()
 
 def compute_metrics(pred):
     labels = pred.label_ids
@@ -33,14 +37,66 @@ def tokenize_function(examples):
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
-max_length = 50
-early_stopping_patience = 10
+parser.add_argument(
+    "-d", 
+    "--dataset", 
+    help="Input dataset to estimate physicochemical properties", 
+    required=True)
 
-df_data = pd.read_csv(sys.argv[1])
-test_size = float(sys.argv[2])
-random_seed = int(sys.argv[3])
-path_export = sys.argv[4]
-model_name = sys.argv[5]
+parser.add_argument(
+    "-s", 
+    "--test_size", 
+    help="Test size dataset", 
+    type=float,
+    default=0.2)
+
+parser.add_argument(
+    "-r", 
+    "--random_seed", 
+    help="Random seed", 
+    type=int,
+    default=42)
+
+parser.add_argument(
+    "-o", 
+    "--path_output", 
+    help="Path to save result", 
+    required=True)
+
+parser.add_argument(
+    "-m", 
+    "--model_name", 
+    help="Name model to use", 
+    required=True)
+
+parser.add_argument(
+    "-l", 
+    "--max_lenght", 
+    help="Max length of sequences", 
+    type=int,
+    default=1024)
+
+parser.add_argument(
+    "-e", 
+    "--early_stop", 
+    help="Early stop tolerance", 
+    type=int,
+    default=10)
+
+args = parser.parse_args()
+
+df_data = pd.read_csv(args.dataset)
+test_size = args.test_size
+random_seed = args.random_seed
+path_export = args.path_output
+model_name = f"ElnaggarLab/{args.model_name}"
+max_length = args.max_lenght
+early_stopping_patience = args.early_stop
+
+df_pos = df_data[df_data["label"] == 1]
+df_neg = df_data[df_data["label"] == 0]
+df_neg = shuffle(df_neg, n_samples=len(df_pos), random_state=random_seed)
+df_data = pd.concat([df_pos, df_neg], axis=0)
 
 name_model = model_name.split("/")[-1]
 
@@ -59,20 +115,20 @@ train_dataset = train_test_split['train']
 eval_dataset = train_test_split['test']
 
 training_args = TrainingArguments(
-    output_dir=f"{path_export}explored_checkpoints",
+    output_dir=f"{path_export}explored_checkpoints_{name_model}",
     evaluation_strategy="epoch",
     save_strategy="epoch",
     learning_rate=2e-5,
     per_device_train_batch_size=4,
     per_device_eval_batch_size=4,
-    num_train_epochs=30,
+    num_train_epochs=50,
     weight_decay=0.01,
     gradient_accumulation_steps=4,
     fp16=True,
     load_best_model_at_end=True,
     metric_for_best_model="f1",
     greater_is_better=True,
-    logging_dir=f"{path_export}logs",
+    logging_dir=f"{path_export}logs_{name_model}",
     logging_steps=10,
     save_total_limit=3,
 )
@@ -90,7 +146,7 @@ trainer.train()
 
 eval_results = trainer.evaluate()
 
-with open(f'{path_export}performances.json', 'w', encoding='utf-8') as f:
+with open(f'{path_export}performances_{name_model}.json', 'w', encoding='utf-8') as f:
     dump(eval_results, f, ensure_ascii=False, indent=4)
 
 model.save_pretrained(f"{path_export}finetuned_{name_model}")
