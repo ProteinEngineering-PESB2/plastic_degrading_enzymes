77af9d46cd985d266e38f6d1701f9a903de9dfe7
dMedinaO <david.medina@cebib.cl>
2024-12-13 13:27:13 +0100

    update repo

---
+++ src/generating_new_enzymes/run_gen3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# 
+#$ -cwd
+#$ -N generating_3
+#$ -q GPU
+#$ -l slotsGPU_1=0
+#$ -l slotsGPU_0=1
+#$ -l memGPU=24
+#$ -pe parallelGPU 1
+
+module load cuda/12.2
+source /raid/data/dmedina/miniconda3/etc/profile.d/conda.sh
+conda activate protein_engineering
+
+sh /home/dmedina/Desktop/projects/plastic_degrading_enzymes/src/generating_new_enzymes/run_generation_enzymes.sh 3.1.1.101 0
---
+++ src/generating_new_enzymes/run_generation_enzymes.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+python3 /home/dmedina/Desktop/projects/plastic_degrading_enzymes/src/generating_new_enzymes/generating_enzymes.py $1 $2
+
